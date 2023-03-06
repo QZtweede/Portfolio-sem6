2a79d1aa459dc2ec9038a4fa5e1aed26c146e9c9
zjuul <zjuulrovers@fruitcake.nl>
2023-03-06 16:01:09 +0100

    wip

--- themes/custom/zjuuls_theme/js/custom.js
+++ themes/custom/zjuuls_theme/js/custom.js
@@ -2,4 +2,16 @@
   $('.menu-toggle').click(function(){
     $('.menu').toggleClass( '-active' );
   })
+  $(window).mousemove(function( event ) {
+    var styles_img = {
+      "top" : event.pageY - 80,
+      "left" : event.pageX - 80
+    }
+    $('.mouse-overlay--img').css(styles_img)
+    var styles_link = {
+      "top" : event.pageY - 60,
+      "left" : event.pageX - 40
+    }
+    $('.mouse-overlay--link').css(styles_link)
+  })
 })(jQuery, Drupal);
