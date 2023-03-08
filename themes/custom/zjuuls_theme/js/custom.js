bebf1d879ef4e2344005107e841b78ebd97de0f2
zjuul <zjuulrovers@fruitcake.nl>
2023-03-08 14:48:01 +0100

    wip

--- themes/custom/zjuuls_theme/js/custom.js
+++ themes/custom/zjuuls_theme/js/custom.js
@@ -2,6 +2,9 @@
   $('.menu-toggle').click(function(){
     $('.menu').toggleClass( '-active' );
   })
+  $('.filter-toggle').click(function(){
+    $('.form-radios').toggleClass( '-active' );
+  })
   $(window).mousemove(function( event ) {
     var styles_img = {
       "top" : event.pageY - 80,
@@ -13,5 +16,14 @@
       "left" : event.pageX - 40
     }
     $('.mouse-overlay--link').css(styles_link)
+    console.log()
+    var styles_interactive = {
+      "transform" : "translate3d(" + (event.pageX - $(window).width() / 2) / 100 + "px, " + (event.pageY - $(window).height() / 2)  / 100 + "px, 0)",
+    }
+    $('.mouse-interactive').css(styles_interactive)
+    var styles_interactive_r = {
+      "transform" : "translate3d(" + (event.pageX - $(window).width() / 2) / 100 * -1 + "px, " + (event.pageY - $(window).height() / 2)  / 100 * -1+ "px, 0)",
+    }
+    $('.mouse-interactive-r').css(styles_interactive_r)
   })
 })(jQuery, Drupal);
